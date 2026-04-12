397002884433e4fb2c50db719988f11a2e13a907
Rulif Fadria Nirwansyah <ruliffax@gmail.com>
2026-04-12 23:18:21 +0700

    fix: update image remotePatterns for production

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
   images: {
-    domains: ['image.tmdb.org'],
-  }
-  /* config options here */
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com", // foto profil Google login
+      },
+    ],
+  },
 };
-
 
 export default nextConfig;
