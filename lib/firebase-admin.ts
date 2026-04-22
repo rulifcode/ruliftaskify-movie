1dd8bb0d89417e34a7302b49a3370928a2aad672
Rulif Fadria Nirwansyah <ruliffax@gmail.com>
2026-04-22 10:53:02 +0700

    revisi1 firebase-admin

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -2,11 +2,15 @@ import { initializeApp, getApps, cert } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 
 if (!getApps().length) {
+  const privateKey = process.env.FIREBASE_PRIVATE_KEY?.includes("\\n")
+    ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")  // format \n literal
+    : process.env.FIREBASE_PRIVATE_KEY;                        // sudah multiline asli
+
   initializeApp({
     credential: cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      privateKey: privateKey,
     }),
   });
 }
